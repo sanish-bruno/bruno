8f40ce4173adc1efc2dd7f826c7bf102f87ce7be
sanish-bruno <sanish@usebruno.com>
2025-03-24 20:52:57 +0530

    Add gRPC support
    Enhance gRPC client documentation to clarify handling of large integers
    
    Refactor gRPC client: remove unnecessary console log and simplify method parameters
    
    Add detailed documentation for startConnection method in gRPC client
    
    Update gRPC client documentation to specify the type of the client instance in connection options
    
    Refactor gRPC request handling: streamline protoPath updates and add checks for duplicate saves, http requests fails to save
    
    Enhance gRPC query handling: implement debounced URL change handling, update loadGrpcMethodsFromReflection to accept URL parameter, and streamline method loading logic.
    
    Implement gRPC request validation: add checks for valid server URL and method selection in GrpcRequestPane and HotkeysProvider, and propagate handleRun function through components.
    
    Enhance gRPC message handling: update GrpcAuth component to include save functionality and refactor Bru grammar to support new body:grpc:message structure for improved message validation and serialization.

--- packages/bruno-app/src/utils/collections/index.js
+++ packages/bruno-app/src/utils/collections/index.js
@@ -241,6 +241,8 @@ export const transformCollectionToSaveToExportAsFile = (collection, options = {}
         di.request = {
           url: si.request.url,
           method: si.request.method,
+          methodType: si.request.methodType,
+          protoPath: si.request.protoPath,
           headers: copyHeaders(si.request.headers),
           params: copyParams(si.request.params),
           body: {
@@ -252,7 +254,8 @@ export const transformCollectionToSaveToExportAsFile = (collection, options = {}
             sparql: si.request.body.sparql,
             formUrlEncoded: copyFormUrlEncodedParams(si.request.body.formUrlEncoded),
             multipartForm: copyMultipartFormParams(si.request.body.multipartForm),
-            file: copyFileParams(si.request.body.file)
+            file: copyFileParams(si.request.body.file),
+            grpc: si.request.body.grpc
           },
           script: si.request.script,
           vars: si.request.vars,
@@ -396,6 +399,13 @@ export const transformCollectionToSaveToExportAsFile = (collection, options = {}
 
         if (di.request.body.mode === 'json') {
           di.request.body.json = replaceTabsWithSpaces(di.request.body.json);
+        }
+
+        if (di.request.body.mode === 'grpc') {
+          di.request.body.grpc = di.request.body.grpc.map(({name, content}, index) => ({
+            name: name ? name : `message ${index + 1}`,
+            content: replaceTabsWithSpaces(content)
+          }))
         }
       }
 
@@ -549,6 +559,9 @@ export const transformCollectionToSaveToExportAsFile = (collection, options = {}
 
 export const transformRequestToSaveToFilesystem = (item) => {
   const _item = item.draft ? item.draft : item;
+
+  console.log('>> item.draft', item.draft);
+
   const itemToSave = {
     uid: _item.uid,
     type: _item.type,
@@ -570,6 +583,11 @@ export const transformRequestToSaveToFilesystem = (item) => {
       docs: _item.request.docs
     }
   };
+
+  if (_item.type === 'grpc-request') {
+    itemToSave.request.methodType = _item.request.methodType;
+    itemToSave.request.protoPath = _item.request.protoPath;
+  }
 
   each(_item.request.params, (param) => {
     itemToSave.request.params.push({
@@ -596,6 +614,18 @@ export const transformRequestToSaveToFilesystem = (item) => {
     itemToSave.request.body = {
       ...itemToSave.request.body,
       json: replaceTabsWithSpaces(itemToSave.request.body.json)
+    };
+  }
+
+  if (itemToSave.request.body.mode === 'grpc') {
+    itemToSave.request.methodType = _item.request.methodType;
+    itemToSave.request.protoPath = _item.request.protoPath;
+    itemToSave.request.body = {
+      ...itemToSave.request.body,
+      grpc: itemToSave.request.body.grpc.map(({name, content}, index) => ({
+        name: name ? name : `message ${index + 1}`,
+        content: replaceTabsWithSpaces(content)
+      }))
     };
   }
 
@@ -626,7 +656,7 @@ export const deleteItemInCollectionByPathname = (pathname, collection) => {
 };
 
 export const isItemARequest = (item) => {
-  return item.hasOwnProperty('request') && ['http-request', 'graphql-request'].includes(item.type) && !item.items;
+  return item.hasOwnProperty('request') && ['http-request', 'graphql-request', 'grpc-request'].includes(item.type) && !item.items;
 };
 
 export const isItemAFolder = (item) => {
